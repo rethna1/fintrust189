77c9d0ba49d6382b13b2d8d58a83ad4988ab1e9b
rethna1 <ret.star2001@gmail.com>
2026-04-26 03:49:33 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = "fintrust-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("activate", event => {
+  return self.clients.claim();
 });
