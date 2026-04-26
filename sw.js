b922fcccde2e6725b6ccbcaa46b26472070d0bc4
rethna1 <ret.star2001@gmail.com>
2026-04-25 23:31:53 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,4 +2,8 @@ self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
+self.addEventListener("activate", e => {
+  return self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {});
